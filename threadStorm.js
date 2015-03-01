c54cd66684e3a0ef3522be578a0cd23317eef49e
charliegarrison <charlieg91@gmail.com>
2015-02-28 23:30:33 -0500

    initial commit
    The first commit, with basic functionality.

---
+++ threadStorm.js
@@ -0,0 +1,156 @@
+var events = require('events'),
+  eventEmitter = new events.EventEmitter(),
+  threadStorm=module.exports={},
+  ee = threadStorm.ee = eventEmitter,
+  cluster = require('cluster');
+
+
+
+threadStorm.isMaster = cluster.isMaster;
+if(cluster.isMaster) {
+  var workers={},
+    availableWorkers=[],
+   CPUs = require('os').cpus(),
+   taskMapping={};
+
+   threadStorm.start = function() {
+     registerListeners();
+     waitForForking();
+
+     CPUs.forEach(function() {
+       cluster.fork();
+     });
+   };
+
+
+   threadStorm.runTask = function(task,data) {
+     var msgObj,
+      msg;
+
+     if(availableWorkers.length > 0) {
+       msg = "Running Worker";
+       msgObj = {
+         status: 1,
+         msg:msg
+       };
+       var worker = workers[availableWorkers[0]];
+
+       taskMapping[worker.id]=task;
+       setWorkerUnAvailable(worker);
+
+       worker.send({
+         cmd: 'run',
+         task: task,
+         data: data
+       });
+     }
+     else {
+       msg = "Currently there are no available workers to run " + task + ". What a sham.";
+       msgObj = {
+         status: 0,
+         msg:msg
+       };
+       console.log(msg);
+     }
+   };
+
+    setWorkerAvailable = function(worker) {
+      var index = availableWorkers.indexOf(worker.id);
+      if(index === -1) {
+        availableWorkers.push(worker.id);
+        console.log("worker " + worker.id + " is ready for work");
+      }
+    };
+
+    setWorkerUnAvailable = function(worker) {
+      var index = availableWorkers.indexOf(worker.id);
+
+      if(index > -1) {
+        availableWorkers.splice(index,index+1);
+        console.log("worker " + worker.id + " has been removed from available workers");
+      }
+
+    };
+
+    registerListeners = function() {
+      cluster.on('fork',function(worker) {
+
+      });
+
+      cluster.on('online',function(worker) {
+        workers[worker.id]=worker;
+
+        //console.log("my name is worker "+worker.id+", and I am online");
+
+        setWorkerAvailable(worker);
+
+        worker.on('message', function(msgObj) {
+          if(msgObj.msg === "completed") {
+            console.log("worker " + worker.id + " has completed running your code! TURN UP!");
+            setWorkerAvailable(worker);
+            ee.emit('taskComplete',{task: taskMapping[worker.id]});
+          }
+          else {
+            ee.emit('msg',msgObj);
+          }
+        });
+      });
+
+      cluster.on('listening',function(worker,address) {
+        console.log("worker is listening");
+      });
+
+      cluster.on('disconnect', function(worker) {
+        console.log("worker " + worker.id + " has disconnected. The nerve...");
+        taskRunner.setWorkerUnAvailable(worker);
+      });
+
+      cluster.on('exit', function(worker, code, signal) {
+        console.log("worker " + worker.process.pid + " died. It was a massacre");
+        taskRunner.setWorkerUnAvailable(worker);
+      });
+
+
+    };
+
+    waitForForking = function() {
+      setTimeout(function() {
+        if(Object.keys(workers).length < CPUs.length) {
+          console.log("Not all of the workers are up yet. No worries, going to keep waiting for these slackers workers to come online. You can sit back and watch Dragon Ball Z");
+          taskRunner.waitForForking();
+        }
+        else {
+          ee.emit("ready");
+          console.log("All workers are up and running. Let's put them to work.");
+          //taskRunner.loop();
+        }
+      },1000);
+    };
+
+
+
+}
+else if(cluster.isWorker) {
+  var worker = cluster.worker,
+    i=0,
+    taskCommunicator,
+    taskToRun;
+
+  //can be called from task
+  module.exports.taskCommunicator = function(msgObj) {
+    process.send(msgObj);
+  }
+
+  //you can call when your task is completed
+  module.exports.completed = function(msgObj) {
+    process.send({msg: 'completed'});
+  }
+
+  process.on('message', function(msgObj) {
+    if(msgObj.cmd === "run") {
+      console.log("worker " + worker.id +  " here, about run a task. Thank you for this opportunity, I a pretty stoked. :)");
+      taskToRun=require(msgObj.task);
+      taskToRun(msgObj.data,module.exports);
+    }
+  });
+}
